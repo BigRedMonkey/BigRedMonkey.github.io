33d3739088b49500405490abe1f9b44a66ae9e68
BigRedMonkey <m13552555033@163.com>
2016-11-16 23:02:25 +0800

    Gzd jl

---
+++ JL/data/timeAxis.js
@@ -0,0 +1,46 @@
+
+/*		工作时光轴
+	*   {
+	*       startTime: @value String            *         工作开始时间  格式为：2016.10.09
+	*       endTime: @value String 			    *         工作结束时间  格式为：2016.10.09
+	*       companyLogo: @value String 	        *         公司log
+			companyName: @value String          *         公司名称
+	*       companyWebsite: @value String       []        公司网址       格式为：http://www.miaov.com
+	*       jobPost: @value String              *         工作职位
+	*       jonTask: @value String       		*         负责任务
+	*       jobContent: @value String         	*         工作具体内容
+	*
+	*
+	*   }
+*/
+
+var timeAxisArr = [
+		{
+		    startTime:"2014.07.01",
+			endTime:"至今",
+			companyLogo:'images/icon/expre_logo4.png',
+			companyNam:"新龙科技集团",
+			companyWebsite:"",
+			jobPost:"前端工程师",
+			jonTask:"负责公司产品设计细节完善和前端制作、优化以及互动效果实现",
+			jobContent:"根据设计网页，并进行页面切片及制作，优化代码并负责产品页面在不同浏览器下的兼容性调试和修改，使用JavaScript、HTML、CSS等技术实现前端交互效果。",
+		},
+		{
+		    startTime:"2015.04.01",
+			endTime:"2015.11.01",
+			companyLogo:'images/icon/expre_logo4.png',
+			companyNam:"中科天博软件技术有限公司",
+			companyWebsite:"",
+			jobPost:"前端工程师",
+			jonTask:"负责公司产品设计细节完善和前端制作、优化以及互动效果实现",
+			jobContent:"根据设计网页，并进行页面切片及制作，优化代码并负责产品页面在不同浏览器下的兼容性调试和修改，使用JavaScript、HTML、CSS等技术实现前端交互效果。",
+		},
+		{
+		    startTime:"2015.11.01",
+			endTime:"至今",
+			companyNam:"华阳创美",
+			companyLogo:'images/icon/expre_logo4.png',
+			companyWebsite:"",
+			jobContent:"根据设计网页，并进行页面切片及制作，优化代码并负责产品页面在不同浏览器下的兼容性调试和修改，使用JavaScript、HTML、CSS等技术实现前端交互效果。",
+		}
+]
