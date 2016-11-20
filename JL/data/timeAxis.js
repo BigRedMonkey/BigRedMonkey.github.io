0318f6a9c6c5120d25c73c7415020b6837d0be1e
BigRedMonkey <m13552555033@163.com>
2016-11-20 20:49:59 +0800

    gzd

--- JL/data/timeAxis.js
+++ JL/data/timeAxis.js
@@ -18,7 +18,7 @@ var timeAxisArr = [
 		{
 		    startTime:"2014.07.01",
 			endTime:"至今",
-			companyLogo:'images/icon/expre_logo4.png',
+			companyLogo:'images/icon/xinlong.png',
 			companyNam:"新龙科技集团",
 			companyWebsite:"",
 			jobPost:"前端工程师",
@@ -28,7 +28,7 @@ var timeAxisArr = [
 		{
 		    startTime:"2015.04.01",
 			endTime:"2015.11.01",
-			companyLogo:'images/icon/expre_logo4.png',
+			companyLogo:'images/icon/zb.png',
 			companyNam:"中科天博软件技术有限公司",
 			companyWebsite:"",
 			jobPost:"前端工程师",
@@ -39,7 +39,7 @@ var timeAxisArr = [
 		    startTime:"2015.11.01",
 			endTime:"至今",
 			companyNam:"华阳创美",
-			companyLogo:'images/icon/expre_logo4.png',
+			companyLogo:'images/icon/huayang.png',
 			companyWebsite:"",
 			jobContent:"根据设计网页，并进行页面切片及制作，优化代码并负责产品页面在不同浏览器下的兼容性调试和修改，使用JavaScript、HTML、CSS等技术实现前端交互效果。",
 		}
